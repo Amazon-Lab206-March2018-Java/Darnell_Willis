fb0e1665c22413bed56907be26034c4d7e9523fb
Darnell <darnellwillis96@gmail.com>
2018-04-23 11:58:42 -0700

    FizzBuzz

---
+++ activiates/FizzBuzzTest.java
@@ -0,0 +1,9 @@
+
+public class FizzBuzzTest {
+            
+    public static void main(String[] args) {
+        FizzBuzz buzz = new FizzBuzz();
+        System.out.println(buzz.fizzBuzz(98));
+    }
+
+}
